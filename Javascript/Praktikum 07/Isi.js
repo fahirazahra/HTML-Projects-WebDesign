78d741bed8ddbadc57ad58c13f58fb32652248ad
Fahira Zahra <fahirazahra@student.uns.ac.id>
2021-04-27 03:06:29 +0700

    Create Isi.js

---
+++ Javascript/Praktikum 07/Isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=blue>");
+document.writeln("<h1>Belajar Javascript</h1>");
+document.writeln("<h2>PTIK UNS 2020</h2>");
+document.writeln("<h4>NIM K3520025</h4>");
+document.writeln("<hr width=600 color=brown>");
+document.writeln("</center>");
